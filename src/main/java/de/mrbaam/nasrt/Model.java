6bf5ba0692fce295a1d2aad4a69f978d129b783f
mrbaam <mrbaam@yahoo.de>
2015-07-18 19:56:11 +0200

    Model added

--- src/main/java/de/mrbaam/nasrt/Model.java
+++ src/main/java/de/mrbaam/nasrt/Model.java
@@ -1,8 +1,35 @@
 package de.mrbaam.nasrt;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  * Created by mrbaam on 17.07.2015.
  * @author mrbaam
  */
 public class Model {
+    private final ObservableList<Path> l_releases;
+
+
+    public Model() {
+        l_releases = FXCollections.observableArrayList();
+    }
+
+
+    public void readReleases(Path rootPath) throws IOException {
+        final ReleaseFileVisitor l_visitor;
+
+        l_visitor = new ReleaseFileVisitor(l_releases);
+
+        Files.walkFileTree(rootPath, l_visitor);
+    }
+
+
+    public ObservableList<Path> getReleases() {
+        return l_releases;
+    }
 }
